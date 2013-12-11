7cb7dc29d44bb69de2d8c7e5552af89d3125756a
James Harvey <james@webmarkdesigns.com.au>
2013-12-11 11:12:07 +1100

    Made settings.php a symlink

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,16 +1,1 @@
-<?php
-
-$opsworks_file = DRUPAL_ROOT . '/opsworks.php';
-if (file_exists($opsworks_file)) {
-  require_once($opsworks_file);
-  $db = new OpsWorksDb();
-  $databases['default']['default'] = array(
-    'database' => $db->database,
-    'username' => $db->username,
-    'password' => $db->password,
-    'host' => $db->host,
-    'port' => '',
-    'driver' => 'mysql',
-    'prefix' => '',
-  );
-}
+../../../../../shared/settings.php
